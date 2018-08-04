430e9f32222066822d9537072858cdfadf953368
Rui Jiang <me@ruijiang.me>
2018-08-04 10:06:48 -0700

    updated greeting message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception 
   def hello
-    render html: "Hello, world from Azure App Service on Linux!"
+    render html: "Hello world!\nThis site is currently undergoing construction.\nPlease check back later."
   end
 end
